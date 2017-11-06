aecda0c8542c44feee7d1cfd1c40fef81a4eaad6
TranBean fool <tranbean007@gmail.com>
2017-11-07 06:52:04 +0900

    Initial commit

---
+++ app/src/main/java/com/umonfly/tranbean/umonfly/bean/DateInfo.java
@@ -0,0 +1,25 @@
+package com.umonfly.tranbean.umonfly.bean;
+
+/**
+ * Created by tranbean on 2017/10/28.
+ */
+public class DateInfo {
+    private String Date;
+    private String week;
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+}
---
+++ app/src/main/java/com/umonfly/tranbean/umonfly/bean/RoomInfo.java
@@ -0,0 +1,34 @@
+package com.umonfly.tranbean.umonfly.bean;
+
+/**
+ * Created by tranbean on 2017/10/28.
+ */
+public class RoomInfo {
+    private String roomType;
+    private String roomName;
+    private long roomId;
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+}
